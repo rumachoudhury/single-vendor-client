e8cd73ec54562c21ea3de861c05082d0bd40d725
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-08-28 01:09:03 -0400

    Update navbar

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -204,7 +204,7 @@ export function Navbar({
             <div className=" flex md:hidden lg:hidden w-full">
               <form onSubmit={handleSearch} className="relative w-full">
                 <Search className="absolute left-2 top-1/2 transform -translate-y-1/2 h-4 w-4 text-muted-foreground" />
-                {/* <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-blue-500" /> */}
+                
                 <Input
                   type="text"
                   placeholder="Search products..."
